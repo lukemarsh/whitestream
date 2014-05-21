c860ed7e8a1f43776bb9c29029d1f1cc5dd9ee29
Luke Marsh <luke@theappbusiness.com>
2014-05-21 13:56:18 +0100

    lightbox

--- app/assets/javascripts/stream.js
+++ app/assets/javascripts/stream.js
@@ -6,8 +6,9 @@ jQuery(function() {
   });
   $('.lightbox').click(function(e) {
     $('.modal-meme').attr('src', $(this).data('image').image.url);
-    $('.fb-share-button, .fb-like').attr('data-href', $(this).data('url'));
+    $('.fb-share-button, .fb-like, .g-plusone').attr('data-href', $(this).data('url'));
     $('.twitter-share-button').attr('data-url', $(this).data('url'));
+    $('.tumblr-share-button').attr('href', 'http://www.tumblr.com/share/link?url=' + $(this).data('url'));
     $('#myModal').modal('show');
     e.preventDefault();
   });
