a20a3c9d9b4fae655216f20102f4a368d319b598
Luke Marsh <luke@theappbusiness.com>
2014-05-21 15:46:00 +0100

    lightbox

--- app/assets/javascripts/stream.js
+++ app/assets/javascripts/stream.js
@@ -6,9 +6,19 @@ jQuery(function() {
   });
   $('.lightbox').click(function(e) {
     $('.modal-meme').attr('src', $(this).data('image').image.url);
-    $('.fb-share-button, .fb-like, .g-plusone').attr('data-href', $(this).data('url'));
-    $('.twitter-share-button').attr('data-url', $(this).data('url'));
-    $('.tumblr-share-button').attr('href', 'http://www.tumblr.com/share/link?url=' + $(this).data('url'));
+
+    var toolbox = $('<div />').addClass('addthis_toolbox addthis_default_style addthis_20x20_style').attr('addthis:url', $(this).data('url')).attr('addthis:title', 'test'),
+      services = {facebook: 'facebook', twitter: 'twitter', google: 'google', tumblr: 'tumblr', reddit: 'reddit'};
+
+    for (var s in services) {
+      toolbox.append('<a class="addthis_counter_'+s+' addthis_native_counter_parent">'+services[s]+'</a>');
+    }
+
+    $("#toolbox").html(toolbox);
+
+    addthis.toolbox("#toolbox");
+
+
     $('#myModal').modal('show');
     e.preventDefault();
   });
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,4 +12,14 @@ class Comment < ActiveRecord::Base
 
   # NOTE: Comments belong to a user
   belongs_to :user
+
+  def avatar_url(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      default_url = "#{root_url}images/guest.png"
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+    end
+  end
 end
