322adfe8e48b76d45365aabef18a8e86ee4cd220
Luke Marsh <luke@theappbusiness.com>
2014-06-02 07:58:33 +0100

    latest changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 categories = Category.create([{ name: 'News' }, { name: 'Showbiz' }, { name: 'Fashion' }, { name: 'Music' }, { name: 'Ideas' }])
 admins = Admin.create!({:email => 'lukeamarsh@gmail.com', :password => 'smurfitt', :password_confirmation => 'smurfitt'})
+admins = Admin.create!({:email => 'we.worldedit@gmail.com', :password => 'hellomate', :password_confirmation => 'hellomate'})
