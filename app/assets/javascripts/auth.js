d4018838b8bb147eab2a7e2cf97c1f13ff281ca2
Luke Marsh <luke@theappbusiness.com>
2014-06-05 16:38:34 +0100

    styling popup

--- app/assets/javascripts/auth.js
+++ app/assets/javascripts/auth.js
@@ -1,11 +1,14 @@
-jQuery(function() {
+jQuery(function () {
   $('body').on('hidden.bs.modal', '.modal', function () {
     $(this).removeData('bs.modal');
   });
-  $('body').on('shown.bs.modal', '.modal', function() {
-    $('.modal-content a').click(function() {
+  $('body').on('shown.bs.modal', '.modal', function () {
+    $('.modal-content a').click(function () {
       var modalToOpen = $(this).data('target');
       $('.modal').not(modalToOpen).modal('hide');
+    });
+    $("#continue").on('click', function () {
+      $("#download").trigger("click", "continueSubmission");
     });
   });
 });
